661b9c73f8566034dd2caa37b8e79162a7396bea
Ben Weller <ben.m.weller@gmail.com>
2018-04-12 15:13:54 -0500

    I have gotten it to recursivly call, and limit it self a bit?

--- tutorial/spiders/emails_spider.py
+++ tutorial/spiders/emails_spider.py
@@ -10,7 +10,7 @@ from scrapy.spiders import CrawlSpider, Rule
 def write_email_output(email, url):
     myData = []
     #Check to see if it's a duplicate
-    with open('trial.csv', 'w') as myFile:
+    with open('output.csv', 'w+') as myFile:
         writer = csv.writer(myFile)
         myData.append([email, url])
         writer.writerows(myData)
@@ -26,22 +26,43 @@ def check_csv_for_duplicates(email, path):
         trial_set_length = len(trial_set)
         return not trial_set_length == len(trial_list) #If the element is new this will return true, if it's a copy it'll return false
 
+def get_initial_urls():
+    urls = []
+    # path = input("Please pass the path of the .csv file that contains your URLs: ")
+    path = "C:\\Users\\Owner\\Documents\\Projects\\CSCI415Project\\tutorial\\trial.csv"
+    with open(path, 'r') as csvfile:
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            for entry in row:
+                if entry:
+                    urls.append(entry)
+    return urls
+
+
+def capture_domains(list_of_urls):
+    returned = []
+    for x in list_of_urls:
+        regular_expression_search_result = re.search(r"https://|http://", x)
+        if regular_expression_search_result:
+            returned.append(x[regular_expression_search_result.end():])
+    return returned
+
+
+
 class EmailsSpider(CrawlSpider):
     name = "Emails"
-    depth = 100
     path = None;
-    urls = []
+    urls = get_initial_urls()
+    allowed_domains = capture_domains(urls)
+    # print(allowed_domains)
+    custom_settings = {
+        'DEPTH_LIMIT': '3',
+    }
 
-    rules = [Rule(LinkExtractor(), follow=True, callback="parse_emails")]
+
+    rules = [Rule(LinkExtractor(allow=allowed_domains, unique=True), callback="parse_emails")]
 
     def start_requests(self):
-        self.path = input("Please pass the path of the .csv file that contains your URLs: ")
-        with open(self.path, 'r') as csvfile:
-            spamreader = csv.reader(csvfile)
-            for row in spamreader:
-                for entry in row:
-                    if entry:
-                        self.urls.append(entry)
         for url in self.urls:
             yield scrapy.Request(url=url, callback=self.parse_emails)
 
@@ -49,30 +70,25 @@ class EmailsSpider(CrawlSpider):
 
 
     def parse_emails(self, response):
-        self.depth -=1
-        # If we've exceeded our depth limited search, then return.
-        if self.depth<0:
-            return
-
         soup = BeautifulSoup(response.text, 'html.parser')
         anchor_list = soup.find_all("a")
 
         # Little bit of defensive programming here, what if there are not links on the page??
         if not anchor_list:
             return
-
+        returned = []
         for tag in anchor_list:
+            print (f"We are in {response.url}")
             if "href" in tag.attrs.keys(): #Grabs the atrributes of that tag as a dictionary
-                result = re.search(r"mailto:", tag["href"])
-                if result:
+                email_result = re.search(r"mailto:", tag["href"])
+                sanity_result = re.search(r"http://|https://", tag["href"])
+                if email_result:
                     # Prepare the email to be written to the CSV
-                    email = tag["href"][result.end():]
-                    # print("Our email is ",email)
-                    write_email_output(email, response.url)
-                    # print("We have found "+str(email)+" on the page with URL, "+str(response.url))
-
-        # page = response.url.split("/")[-2]
-        # filename = 'quotes-%s.html' % page
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s' % filename)
+                    email = tag["href"][email_result.end():]
+                    # write_email_output(email, response.url)
+                    print(f"We would write: {email}, {response.url}")
+                elif sanity_result:
+                    value = tag["href"]
+                    print(f"The URL we will append is: {value}")
+                    returned.append(scrapy.Request(url=tag["href"], callback=self.parse_emails))
+        return returned
